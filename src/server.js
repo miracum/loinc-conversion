dac9cff407795f5c0769108644aef8ae8af36a63
Noemi Deppenwiese <noemi.deppenwiese@uk-erlangen.de>
2023-02-17 16:27:55 +0000

    chore: Upgrade fastify listen method call

--- src/server.js
+++ src/server.js
@@ -331,7 +331,7 @@ const port = process.env.PORT || 8080;
 
 const start = async () => {
   try {
-    await app.listen(port, "0.0.0.0");
+    await app.listen({ port: port, host: '0.0.0.0' });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
